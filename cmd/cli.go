90b2136f66267e76f8ea27ff4423a21587387214
nairo <nairobi93@mail.ru>
2021-02-08 23:25:31 +0300

    gma serve and cli

---
+++ cmd/cli.go
@@ -0,0 +1,102 @@
+/*
+Copyright © 2021 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/spf13/cobra"
+	"io"
+	"log"
+	"net"
+	"os"
+	"strings"
+)
+
+// cliCmd represents the cli command
+var cliCmd = &cobra.Command{
+	Use:   "cli",
+	Short: "Start gma console",
+	Run: func(cmd *cobra.Command, args []string) {
+		startCli()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(cliCmd)
+	log.SetFlags(0)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// cliCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// cliCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func socketReader(r io.Reader) {
+	// TODO: change it
+	buf := make([]byte, 512)
+	for {
+		n, err := r.Read(buf[:])
+		if err != nil {
+			return
+		}
+		fmt.Println(string(buf[0:n]))
+	}
+}
+
+func startCli() {
+	cliReader := bufio.NewReader(os.Stdin)
+
+	client, err := net.Dial("unix", "./tmp/echo.sock")
+	if err != nil {
+		if _, ok := err.(*net.OpError); ok {
+			log.Fatal("Could not connect to Gma server: Connection refused")
+		}
+
+		log.Fatal("Dial error", err)
+	}
+	defer client.Close()
+
+	go socketReader(client)
+
+	for {
+		fmt.Printf("> ")
+		text, _ := cliReader.ReadString('\n')
+		// split the text into operation strings
+		operation := strings.Fields(text)
+		switch operation[0] {
+		case "exit":
+			return
+		case "SET", "GET", "DEL":
+			handleSocketCommand(client, strings.Trim(text, "\n"))
+		//case "DELETE": 		Delete(operation[1], items)
+		default:
+			fmt.Printf("ERROR: Unrecognised Operation %s\n", operation[0])
+		}
+	}
+}
+
+func handleSocketCommand(client net.Conn, text string) {
+	if _, err := client.Write([]byte(text)); err != nil {
+		fmt.Println("Write error:", err)
+		return
+	}
+}
---
+++ cmd/serve.go
@@ -0,0 +1,199 @@
+/*
+Copyright © 2021 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"io"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"strconv"
+	"strings"
+	"syscall"
+)
+
+// serveCmd represents the serve command
+var serveCmd = &cobra.Command{
+	Use:   "serve",
+	Short: "Start gma server",
+	Run: func(cmd *cobra.Command, args []string) {
+		startServer()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(serveCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// serveCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// serveCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func startServer() {
+	// channel for handling *nix termination commands
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+
+	store := make(map[string]string)
+
+	// create unix socket file
+	listener, err := net.Listen("unix", "./tmp/echo.sock")
+	if err != nil {
+		log.Fatal("listen error:", err)
+	}
+
+	defer listener.Close()
+
+	fmt.Println("Gma server has been started successfully")
+
+	listenerChan := make(chan net.Conn)
+
+	go func() {
+		for {
+			connect, err := listener.Accept()
+
+			if err != nil {
+				// TODO: change me
+				if _, ok := err.(*net.OpError); ok {
+					log.Fatal("Connection refused")
+				}
+
+				log.Fatal("accept error: ", err)
+			}
+			listenerChan <- connect
+		}
+	}()
+
+	for {
+		select {
+		case <-signalChan:
+			listener.Close()
+			os.Exit(1)
+		case lol := <-listenerChan:
+			go echoServer(lol, store)
+		}
+	}
+}
+
+func echoServer(connect net.Conn, store map[string]string) {
+	defer connect.Close()
+
+	for {
+		// TODO: change it
+		buf := make([]byte, 512)
+		lastByte, err := connect.Read(buf)
+
+		if err == io.EOF {
+			connect.Close()
+			return
+		}
+
+		if err != nil {
+			log.Fatal("handle connect error:", err)
+		}
+
+		data := string(buf[0:lastByte])
+
+		// TODO: strings.Fields(data)
+		switch {
+		// SET lol bol
+		case strings.HasPrefix(data, "SET "):
+			handleSet(data, store, connect)
+		// GET lol bol
+		case strings.HasPrefix(data, "GET "):
+			handleGet(data, store, connect)
+
+		case strings.HasPrefix(data, "DEL "):
+			handleDel(data, store, connect)
+		}
+
+		//_, err = c.Write(data)
+		//if err != nil {
+		//	log.Fatal("Writing client error: ", err)
+		//}
+	}
+}
+
+func handleSet(data string, store map[string]string, connect net.Conn) {
+	arrayKV := strings.Fields(strings.TrimPrefix(data, "SET "))
+
+	if isArgsCntCorrect(arrayKV, "SET", connect) {
+		store[arrayKV[0]] = arrayKV[1]
+		response(connect, "OK")
+	}
+}
+
+func handleGet(data string, store map[string]string, connect net.Conn) {
+	arrayKV := strings.Fields(strings.TrimPrefix(data, "GET "))
+
+	if isArgsCntCorrect(arrayKV, "GET", connect) {
+		result := store[arrayKV[0]]
+		if result == "" {
+			result = "(nil)"
+		}
+		response(connect, result)
+	}
+}
+
+func handleDel(data string, store map[string]string, connect net.Conn) {
+	arrayKeys := strings.Fields(strings.TrimPrefix(data, "DEL "))
+
+	if isArgsCntCorrect(arrayKeys, "DEL", connect) {
+		count := 0
+		for _, key := range arrayKeys {
+			if _, found := store[key]; found {
+				delete(store, key)
+				count++
+			}
+		}
+		response(connect, strconv.Itoa(count))
+	}
+}
+
+func isArgsCntCorrect(commandArgs []string, commandName string, connect net.Conn) bool {
+	var isOk bool
+
+	switch commandName {
+	case "SET":
+		isOk = len(commandArgs) == 2
+	case "GET":
+		isOk = len(commandArgs) == 1
+	case "DEL":
+		isOk = len(commandArgs) > 0
+	}
+
+	if isOk {
+		return true
+	}
+
+	response(connect, fmt.Sprintf("(error) ERR wrong number of arguments for %s command", commandName))
+	return false
+}
+
+func response(connect net.Conn, data string) {
+	if _, err := connect.Write([]byte(data)); err != nil {
+		fmt.Println("Writing client error: ", err)
+	}
+}
